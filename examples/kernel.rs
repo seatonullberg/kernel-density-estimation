4d33c71e49962df18d5133b908ce0f201f1d78d8
Seaton Ullberg <seatonullberg@gmail.com>
2022-10-09 13:07:31 -0400

    Update examples

---
+++ examples/kernel.rs
@@ -0,0 +1,42 @@
+use mvkde::prelude::*;
+
+use plotly::common::Mode;
+use plotly::{Plot, Scatter};
+
+fn main() {
+    let observations: Vec<f64> = vec![1.0, 1.2, 1.4, 1.6, 3.0, 3.4, 3.8];
+    let x1 = observations.clone();
+    let y1 = vec![0.0; observations.len()];
+
+    let bandwidth = Box::new(|_: &[f64]| 1.0);
+
+    let kde1 = KernelDensityEstimator::new(observations.clone(), &bandwidth, Epanechnikov);
+    let kde2 = KernelDensityEstimator::new(observations.clone(), &bandwidth, Normal);
+    let kde3 = KernelDensityEstimator::new(observations.clone(), &bandwidth, Uniform);
+
+    let dataset: Vec<f64> = (0..101)
+        .into_iter()
+        .map(|x| x as f64 * (5. / 100.))
+        .collect();
+
+    let x2 = dataset.clone();
+    let y2 = kde1.pdf(dataset.as_slice());
+
+    let x3 = dataset.clone();
+    let y3 = kde2.pdf(dataset.as_slice());
+
+    let x4 = dataset.clone();
+    let y4 = kde3.pdf(dataset.as_slice());
+
+    let trace1 = Scatter::new(x1, y1).mode(Mode::Markers).name("Data");
+    let trace2 = Scatter::new(x2, y2).mode(Mode::Lines).name("Epanechnikov");
+    let trace3 = Scatter::new(x3, y3).mode(Mode::Lines).name("Normal");
+    let trace4 = Scatter::new(x4, y4).mode(Mode::Lines).name("Uniform");
+
+    let mut plot = Plot::new();
+    plot.add_trace(trace1);
+    plot.add_trace(trace2);
+    plot.add_trace(trace3);
+    plot.add_trace(trace4);
+    plot.show();
+}
--- examples/univariate.rs
+++ examples/univariate.rs
@@ -1,33 +1,54 @@
 use mvkde::prelude::*;
 
-use plotly::common::Mode;
-use plotly::{Plot, Scatter};
+use plotly::color::NamedColor;
+use plotly::common::{Marker, Mode};
+use plotly::{Histogram, Plot, Scatter};
 
 fn main() {
-    let samples: Vec<f64> = vec![
+    let observations: Vec<f64> = vec![
         0.9, 1.0, 1.1, 4.8, 4.9, 5.0, 5.1, 5.2, 8.25, 8.5, 8.75, 9.0, 9.25, 9.5, 9.75,
     ];
-    let x1 = samples.clone();
-    let y1 = vec![0.0; samples.len()];
+    let x1 = observations.clone();
+    let y1 = vec![0.0; observations.len()];
 
-    let kde = KernelDensityEstimator::new(samples);
+    let bandwidth = Scott;
+    let kernel = Epanechnikov;
+    let kde = KernelDensityEstimator::new(observations, bandwidth, kernel);
 
     let pdf_dataset: Vec<f64> = (0..101).into_iter().map(|x| x as f64 * 0.1).collect();
     let cdf_dataset = pdf_dataset.clone();
+    let sample_dataset = cdf_dataset.clone();
 
     let x2 = pdf_dataset.clone();
-    let y2 = kde.pdf(pdf_dataset, Scott, Epanechnikov);
-    
-    let x3 = cdf_dataset.clone();
-    let y3 = kde.cdf(cdf_dataset, Scott, Epanechnikov);
+    let y2 = kde.pdf(pdf_dataset.as_slice());
 
-    let trace1 = Scatter::new(x1, y1).mode(Mode::Markers).name("Data");
-    let trace2 = Scatter::new(x2, y2).mode(Mode::Lines).name("PDF");
-    let trace3 = Scatter::new(x3, y3).mode(Mode::Lines).name("CDF");
+    let x3 = cdf_dataset.clone();
+    let y3 = kde.cdf(cdf_dataset.as_slice());
+
+    let x4 = kde.sample(sample_dataset.as_slice(), 10_000);
+
+    let trace1 = Scatter::new(x1, y1)
+        .mode(Mode::Markers)
+        .marker(Marker::new().color(NamedColor::CornflowerBlue))
+        .name("Data");
+    let trace2 = Scatter::new(x2, y2)
+        .mode(Mode::Lines)
+        .marker(Marker::new().color(NamedColor::Black))
+        .name("PDF");
+    let trace3 = Scatter::new(x3, y3)
+        .mode(Mode::Lines)
+        .marker(Marker::new().color(NamedColor::YellowGreen))
+        .name("CDF");
+    let trace4 = Histogram::new(x4)
+        .hist_norm(plotly::histogram::HistNorm::ProbabilityDensity)
+        .marker(Marker::new().color(NamedColor::Bisque))
+        .n_bins_x(100)
+        .name("Histogram");
 
     let mut plot = Plot::new();
     plot.add_trace(trace1);
     plot.add_trace(trace2);
     plot.add_trace(trace3);
+    plot.add_trace(trace4);
     plot.show();
 }
